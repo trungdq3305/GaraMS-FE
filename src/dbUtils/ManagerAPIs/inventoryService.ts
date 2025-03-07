2113e1023316a8395ff10e84fb9ef0b38eeedc78
trungdq3305 <trungdqse183305@fpt.edu.vn>
2025-03-07 11:30:00 +0700

    inventory/suplier page

---
+++ src/dbUtils/ManagerAPIs/inventoryService.ts
@@ -0,0 +1,33 @@
+import axiosInstance from "../axios";
+
+export const getInventories = async () => {
+    const response = await axiosInstance.get("inventory/inventories");
+    return response.data;
+};
+
+export const addInventory = async (serviceData: {
+    name: number;
+    description: string;
+    unit: string;
+    price: number;
+    status: string;
+}) => {
+    console.log(serviceData);
+    const response = await axiosInstance.post("inventory/inventory", serviceData);
+    return response.data;
+};
+
+export const updateInventory = async (id: number, serviceData: {
+    name: number;
+    description: string;
+    unit: string;
+    price: number;
+    status: string;
+}) => {
+    const response = await axiosInstance.put(`inventory/inventory/${id}`, serviceData);
+    return response.data;
+};
+
+export const deleteInventory = async (id: number) => {
+    await axiosInstance.delete(`inventory/inventory/${id}`);
+};
--- src/dbUtils/ManagerAPIs/serviceservice.ts
+++ src/dbUtils/ManagerAPIs/serviceservice.ts
@@ -6,26 +6,26 @@ export const getServices = async () => {
 };
 
 export const addService = async (serviceData: {
-    serviceId: number;
-    serviceName: string;
-    price: number;
-    description: string;
-  }) => {
-    console.log(serviceData);
-    const response = await axiosInstance.post("service/service", serviceData);
-    return response.data;
-  };
+  serviceId: number;
+  serviceName: string;
+  price: number;
+  description: string;
+}) => {
+  console.log(serviceData);
+  const response = await axiosInstance.post("service/service", serviceData);
+  return response.data;
+};
 
-  export const updateService = async (serviceId: number, serviceData: {
-    serviceId: number;
-    serviceName: string;
-    price: number;
-    description: string;
-  }) => {
-    const response = await axiosInstance.put(`service/service/${serviceId}`, serviceData);
-    return response.data;
-  };
+export const updateService = async (serviceId: number, serviceData: {
+  serviceId: number;
+  serviceName: string;
+  price: number;
+  description: string;
+}) => {
+  const response = await axiosInstance.put(`service/service/${serviceId}`, serviceData);
+  return response.data;
+};
 
-  export const deleteService = async (serviceId: number) => {
-    await axiosInstance.delete(`service/service/${serviceId}`);
-  };
+export const deleteService = async (serviceId: number) => {
+  await axiosInstance.delete(`service/service/${serviceId}`);
+};
